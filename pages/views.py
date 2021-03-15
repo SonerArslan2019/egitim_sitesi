8f5cabd5190334f47ef5beda10f8513be6ff9672
cprm <soner@arslanyapi.com.tr>
2021-03-15 21:13:46 +0300

    son komitler yapildi

--- pages/views.py
+++ pages/views.py
@@ -2,17 +2,12 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views.generic.edit import FormView
 from courses.models import Course
-from .forms import ContactForm
+from . forms import ContactForm
 from django.urls import reverse_lazy
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.models import User
+from teachers.models import Teacher
 
-
-# from django.contrib.auth.models import User
-# from teachers.models import
-
-
-# def index(request):
-#     return render(request, 'index.html')
 
 class IndexView(TemplateView):
     template_name = 'index.html'
@@ -21,15 +16,18 @@ class IndexView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['courses'] = Course.objects.filter(available=True).order_by('-date')[:2]
         context['total_course'] = Course.objects.filter(available=True).count()
+        context['total_students'] = User.objects.count()
+        context['total_teachers'] = Teacher.objects.count()
         return context
 
+#def index(request):
+#    return render(request, 'index.html')
 
 class AboutView(TemplateView):
     template_name = 'about.html'
 
-
-# def about(request):
-#     return render(request, 'about.html')
+#def about(request):
+#   return render(request, 'about.html')
 
 class ContactView(SuccessMessageMixin, FormView):
     template_name = 'contact.html'
